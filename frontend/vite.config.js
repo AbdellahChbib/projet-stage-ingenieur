079abdd9b4302bd473006144d45c2b9f733f0d0a
Abdellah <abdellahchbib20@gmail.com>
2025-07-16 22:13:33 +0100

    configure vite to listen to 0.0.0.0 so to run docker image for frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: '0.0.0.0',     // Nécessaire pour que Vite soit accessible depuis Docker
+    port: 5173,
+    strictPort: true
+  }
 })
+ 
