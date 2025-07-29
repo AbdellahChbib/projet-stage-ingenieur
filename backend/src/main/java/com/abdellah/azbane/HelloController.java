86ef5aac1c4ca8fc48c674261b365490c36b01f6
Abdellah <abdellahchbib20@gmail.com>
2025-07-29 13:13:55 +0100

    Remove @CrossOrigin from HelloController; handled globally

--- backend/src/main/java/com/abdellah/azbane/HelloController.java
+++ backend/src/main/java/com/abdellah/azbane/HelloController.java
@@ -1,11 +1,9 @@
 package com.abdellah.azbane;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 public class HelloController {
     @GetMapping("/hello")
