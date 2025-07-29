5aeb70792d456816bcf3664c632dcf74a503c8f4
Abdellah <abdellahchbib20@gmail.com>
2025-07-29 13:07:20 +0100

    Fix CORS configuration to allow frontend requests from deployed domain

---
+++ backend/src/main/java/com/abdellah/azbane/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.abdellah.azbane.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://azbane-frontend-app-dycshbffegcpgydk.francecentral-01.azurewebsites.net")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
