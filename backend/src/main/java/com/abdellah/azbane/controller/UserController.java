91632f8b02a73ed1e600aa1c0b540c35ae9daef7
Abdellah <abdellahchbib20@gmail.com>
2025-08-12 00:35:53 +0100

    refactor: remove CORS annotation from UserController

--- backend/src/main/java/com/abdellah/azbane/controller/UserController.java
+++ backend/src/main/java/com/abdellah/azbane/controller/UserController.java
@@ -14,7 +14,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class UserController {
 
     @GetMapping("/profile")
